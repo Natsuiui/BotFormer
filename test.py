ecd91974286b73636f8c981d36914fac6d6cb130
Natsuiui <kshitijsharma1106@gmail.com>
2023-08-27 11:08:42 +0530

    -

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv()
-DISCORD_TOKEN = os.getenv("TOKEN")
+DISCORD_TOKEN = os.getenv("TOKEN", default="/Users/kshitij/Documents/Projects/Discord-Bot-LLMs/Assets")
 print(DISCORD_TOKEN)
